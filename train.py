94b74392e82c4ccd089d2f20ba9501d27ef2b521
Hyejun An <jagaldol.dev@gmail.com>
2024-11-18 13:49:24 +0000

    refactor: `train.py`에 if __name__ == "__main__": 블록 추가

--- train.py
+++ train.py
@@ -10,55 +10,56 @@ from evaluation import compute_metrics, preprocess_logits_for_metrics
 from models import load_model_and_tokenizer
 from utils import set_seed
 
-dotenv.load_dotenv()
 
-# 설정 로드
-config = Config()
+def train(config: Config):
+    # # wandb 초기화
+    # wandb.init(
+    #     project=config.wandb.project,
+    #     entity=config.wandb.entity,
+    #     name=config.wandb.name,
+    #     config=config.raw_config,  # 설정값을 wandb에 로깅
+    # )
 
-set_seed(config.training.seed)
+    model, tokenizer = load_model_and_tokenizer(config.model.name_or_path, config.training.device)
 
-# 모델, 토크나이저 로드 및 설정
-model, tokenizer = load_model_and_tokenizer(config.model.name_or_path, config.training.device)
+    data_loader = DataLoader("data/train.csv", tokenizer, config)
 
-data_loader = DataLoader("data/train.csv", tokenizer, config)
+    response_template = "<start_of_turn>model"
+    data_collator = DataCollatorForCompletionOnlyLM(
+        response_template=response_template,
+        tokenizer=tokenizer,
+    )
+
+    peft_config = create_peft_config(config.peft)
+    sft_config = create_sft_config(config.sft)
+
+    trainer = SFTTrainer(
+        model=model,
+        train_dataset=data_loader.train_dataset,
+        eval_dataset=data_loader.eval_dataset,
+        data_collator=data_collator,
+        tokenizer=tokenizer,
+        compute_metrics=lambda eval_res: compute_metrics(eval_res, tokenizer),
+        preprocess_logits_for_metrics=lambda logits, labels: preprocess_logits_for_metrics(logits, labels, tokenizer),
+        peft_config=peft_config,
+        args=sft_config,
+    )
+    trainer.train()
+
+    # 학습 종료 후 체크포인트 삭제
+    if os.path.exists(sft_config.output_dir):
+        print(f"Deleting checkpoints in {sft_config.output_dir}...")
+        shutil.rmtree(sft_config.output_dir)  # 체크포인트 디렉토리 삭제
+
+    # 최종 모델 저장
+    final_model_dir = "outputs/ko-gemma"
+    print(f"Saving final model to {config.sft.save_dir}...")
+    trainer.save_model(final_model_dir)
 
 
-response_template = "<start_of_turn>model"
-data_collator = DataCollatorForCompletionOnlyLM(
-    response_template=response_template,
-    tokenizer=tokenizer,
-)
+if __name__ == "__main__":
+    dotenv.load_dotenv()
+    config = Config()
+    set_seed(config.training.seed)
 
-# # wandb 초기화
-# wandb.init(
-#     project=config.wandb.project,
-#     entity=config.wandb.entity,
-#     name=config.wandb.name,
-#     config=config.raw_config,  # 설정값을 wandb에 로깅
-# )
-
-peft_config = create_peft_config(config.peft)
-sft_config = create_sft_config(config.sft)
-
-trainer = SFTTrainer(
-    model=model,
-    train_dataset=data_loader.train_dataset,
-    eval_dataset=data_loader.eval_dataset,
-    data_collator=data_collator,
-    tokenizer=tokenizer,
-    compute_metrics=lambda eval_res: compute_metrics(eval_res, tokenizer),
-    preprocess_logits_for_metrics=lambda logits, labels: preprocess_logits_for_metrics(logits, labels, tokenizer),
-    peft_config=peft_config,
-    args=sft_config,
-)
-trainer.train()
-
-# 학습 종료 후 체크포인트 삭제
-if os.path.exists(sft_config.output_dir):
-    print(f"Deleting checkpoints in {sft_config.output_dir}...")
-    shutil.rmtree(sft_config.output_dir)  # 체크포인트 디렉토리 삭제
-
-# 최종 모델 저장
-final_model_dir = "outputs/ko-gemma"
-print(f"Saving final model to {config.sft.save_dir}...")
-trainer.save_model(final_model_dir)
+    train(config)
