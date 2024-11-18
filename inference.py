f9ecbe80f6a6c847eaf0f9ea0ca6d49e4d123f7e
Hyejun An <jagaldol.dev@gmail.com>
2024-11-18 06:43:56 +0000

    refactor: inference 함수로 로직 분리 및 코드 간소화

--- inference.py
+++ inference.py
@@ -4,11 +4,46 @@ import pandas as pd
 import torch
 from peft import AutoPeftModelForCausalLM
 from tqdm import tqdm
-from transformers import AutoTokenizer
+from transformers import AutoTokenizer, PreTrainedModel, PreTrainedTokenizer
 
 from data_loaders import load_data
 from prompts import make_prompt
 from utils import set_seed
+
+
+def inference(model: PreTrainedModel, tokenizer: PreTrainedTokenizer, dataset: list) -> pd.DataFrame:
+    infer_results = []
+    pred_choices_map = {0: "1", 1: "2", 2: "3", 3: "4", 4: "5"}
+
+    model.eval()
+    with torch.inference_mode():
+        for data in tqdm(dataset):
+            _id = data["id"]
+            messages = data["messages"]
+            len_choices = data["len_choices"]
+
+            outputs = model(
+                tokenizer.apply_chat_template(
+                    messages,
+                    tokenize=True,
+                    add_generation_prompt=True,
+                    return_tensors="pt",
+                ).to("cuda")
+            )
+
+            logits = outputs.logits[:, -1].flatten().cpu()
+
+            target_logit_list = [logits[tokenizer.vocab[str(i + 1)]] for i in range(len_choices)]
+
+            probs = (
+                torch.nn.functional.softmax(torch.tensor(target_logit_list, dtype=torch.float32)).detach().cpu().numpy()
+            )
+
+            predict_value = pred_choices_map[np.argmax(probs, axis=-1)]
+            infer_results.append({"id": _id, "answer": predict_value})
+
+    return pd.DataFrame(infer_results)
+
 
 if __name__ == "__main__":
     dotenv.load_dotenv()
@@ -44,35 +79,7 @@ if __name__ == "__main__":
                 "len_choices": len_choices,
             }
         )
-    infer_results = []
 
-    pred_choices_map = {0: "1", 1: "2", 2: "3", 3: "4", 4: "5"}
+    infer_results = inference(model, tokenizer, test_dataset)
 
-    model.eval()
-    with torch.inference_mode():
-        for data in tqdm(test_dataset):
-            _id = data["id"]
-            messages = data["messages"]
-            len_choices = data["len_choices"]
-
-            outputs = model(
-                tokenizer.apply_chat_template(
-                    messages,
-                    tokenize=True,
-                    add_generation_prompt=True,
-                    return_tensors="pt",
-                ).to("cuda")
-            )
-
-            logits = outputs.logits[:, -1].flatten().cpu()
-
-            target_logit_list = [logits[tokenizer.vocab[str(i + 1)]] for i in range(len_choices)]
-
-            probs = (
-                torch.nn.functional.softmax(torch.tensor(target_logit_list, dtype=torch.float32)).detach().cpu().numpy()
-            )
-
-            predict_value = pred_choices_map[np.argmax(probs, axis=-1)]
-            infer_results.append({"id": _id, "answer": predict_value})
-
-    pd.DataFrame(infer_results).to_csv("data/output.csv", index=False)
+    infer_results.to_csv("data/output.csv", index=False)
