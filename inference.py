97c8046af089dc6a62f409b437b32d1e5a47ce8e
Hyejun An <jagaldol.dev@gmail.com>
2024-11-18 05:17:59 +0000

    refactor: if __name__ == "__main__": 블록 추가

--- inference.py
+++ inference.py
@@ -21,9 +21,6 @@ def set_seed(random_seed):
     random.seed(random_seed)
 
 
-dotenv.load_dotenv()
-set_seed(42)
-
 PROMPT_NO_QUESTION_PLUS = """지문:
 {paragraph}
 
@@ -52,83 +49,89 @@ PROMPT_QUESTION_PLUS = """지문:
 정답:"""
 
 
-checkpoint_path = "outputs_gemma/checkpoint-4485"
+if __name__ == "__main__":
+    dotenv.load_dotenv()
+    set_seed(42)
 
-model = AutoPeftModelForCausalLM.from_pretrained(
-    checkpoint_path,
-    trust_remote_code=True,
-    # torch_dtype=torch.bfloat16,
-    device_map="auto",
-)
-tokenizer = AutoTokenizer.from_pretrained(
-    checkpoint_path,
-    trust_remote_code=True,
-)
+    checkpoint_path = "outputs_gemma/checkpoint-4485"
 
-# Load the test dataset
-# TODO Test Data 경로 입력
-test_df = pd.read_csv("data/test.csv")
-test_df["choices"] = test_df["choices"].apply(literal_eval)
-test_df["question_plus"] = test_df["question_plus"].fillna("")
-
-test_dataset = []
-for i, row in test_df.iterrows():
-    choices_string = "\n".join([f"{idx + 1} - {choice}" for idx, choice in enumerate(row["choices"])])
-    len_choices = len(row["choices"])
-
-    # <보기>가 있을 때
-    if row["question_plus"]:
-        user_message = PROMPT_QUESTION_PLUS.format(
-            paragraph=row["paragraph"],
-            question=row["question"],
-            question_plus=row["question_plus"],
-            choices=choices_string,
-        )
-    # <보기>가 없을 때
-    else:
-        user_message = PROMPT_NO_QUESTION_PLUS.format(
-            paragraph=row["paragraph"],
-            question=row["question"],
-            choices=choices_string,
-        )
-
-    test_dataset.append(
-        {
-            "id": row["id"],
-            "messages": [
-                {"role": "system", "content": "지문을 읽고 질문의 답을 구하세요."},
-                {"role": "user", "content": user_message},
-            ],
-            "len_choices": len_choices,
-        }
+    model = AutoPeftModelForCausalLM.from_pretrained(
+        checkpoint_path,
+        trust_remote_code=True,
+        # torch_dtype=torch.bfloat16,
+        device_map="auto",
     )
-infer_results = []
+    tokenizer = AutoTokenizer.from_pretrained(
+        checkpoint_path,
+        trust_remote_code=True,
+    )
 
-pred_choices_map = {0: "1", 1: "2", 2: "3", 3: "4", 4: "5"}
+    # Load the test dataset
+    # TODO Test Data 경로 입력
+    test_df = pd.read_csv("data/test.csv")
+    test_df["choices"] = test_df["choices"].apply(literal_eval)
+    test_df["question_plus"] = test_df["question_plus"].fillna("")
 
-model.eval()
-with torch.inference_mode():
-    for data in tqdm(test_dataset):
-        _id = data["id"]
-        messages = data["messages"]
-        len_choices = data["len_choices"]
+    test_dataset = []
+    for i, row in test_df.iterrows():
+        choices_string = "\n".join([f"{idx + 1} - {choice}" for idx, choice in enumerate(row["choices"])])
+        len_choices = len(row["choices"])
 
-        outputs = model(
-            tokenizer.apply_chat_template(
-                messages,
-                tokenize=True,
-                add_generation_prompt=True,
-                return_tensors="pt",
-            ).to("cuda")
+        # <보기>가 있을 때
+        if row["question_plus"]:
+            user_message = PROMPT_QUESTION_PLUS.format(
+                paragraph=row["paragraph"],
+                question=row["question"],
+                question_plus=row["question_plus"],
+                choices=choices_string,
+            )
+        # <보기>가 없을 때
+        else:
+            user_message = PROMPT_NO_QUESTION_PLUS.format(
+                paragraph=row["paragraph"],
+                question=row["question"],
+                choices=choices_string,
+            )
+
+        test_dataset.append(
+            {
+                "id": row["id"],
+                "messages": [
+                    {"role": "system", "content": "지문을 읽고 질문의 답을 구하세요."},
+                    {"role": "user", "content": user_message},
+                ],
+                "len_choices": len_choices,
+            }
         )
+    infer_results = []
 
-        logits = outputs.logits[:, -1].flatten().cpu()
+    pred_choices_map = {0: "1", 1: "2", 2: "3", 3: "4", 4: "5"}
 
-        target_logit_list = [logits[tokenizer.vocab[str(i + 1)]] for i in range(len_choices)]
+    model.eval()
+    with torch.inference_mode():
+        for data in tqdm(test_dataset):
+            _id = data["id"]
+            messages = data["messages"]
+            len_choices = data["len_choices"]
 
-        probs = torch.nn.functional.softmax(torch.tensor(target_logit_list, dtype=torch.float32)).detach().cpu().numpy()
+            outputs = model(
+                tokenizer.apply_chat_template(
+                    messages,
+                    tokenize=True,
+                    add_generation_prompt=True,
+                    return_tensors="pt",
+                ).to("cuda")
+            )
 
-        predict_value = pred_choices_map[np.argmax(probs, axis=-1)]
-        infer_results.append({"id": _id, "answer": predict_value})
+            logits = outputs.logits[:, -1].flatten().cpu()
 
-pd.DataFrame(infer_results).to_csv("data/output.csv", index=False)
+            target_logit_list = [logits[tokenizer.vocab[str(i + 1)]] for i in range(len_choices)]
+
+            probs = (
+                torch.nn.functional.softmax(torch.tensor(target_logit_list, dtype=torch.float32)).detach().cpu().numpy()
+            )
+
+            predict_value = pred_choices_map[np.argmax(probs, axis=-1)]
+            infer_results.append({"id": _id, "answer": predict_value})
+
+    pd.DataFrame(infer_results).to_csv("data/output.csv", index=False)
