467b13d8987d39cae07d0cddcfc5ae45e01c0928
Hyejun An <jagaldol.dev@gmail.com>
2024-11-20 01:09:53 +0000

    refactor: 최종 prediction 만드는 preprocess 로직 함수 분리

--- evaluation/preprocess.py
+++ evaluation/preprocess.py
@@ -1,3 +1,8 @@
+import numpy as np
+import torch
+from transformers import PreTrainedTokenizerFast
+
+
 def preprocess_logits_for_metrics(logits, labels, logit_idx: list[int]):
     """
     모델의 logits를 조정하여 정답 토큰 부분만 출력하도록 설정하는 함수.
@@ -13,3 +18,28 @@ def preprocess_logits_for_metrics(logits, labels, logit_idx: list[int]):
     logits = logits if not isinstance(logits, tuple) else logits[0]
     logits = logits[:, -2, logit_idx]  # -2: answer token, -1: eos token
     return logits
+
+
+def logits_to_predictions(logits):
+    """
+    logits를 확률로 변환하는 함수.
+
+    Args:
+        logits: 모델의 출력 logits(1, 2, 3, 4, 5에 대해서만 존재).
+    """
+    probs = torch.nn.functional.softmax(torch.tensor(logits), dim=-1)
+    predicted_indices = np.argmax(probs, axis=-1).tolist()
+
+    # 0~4 -> 1~5 변환 (문자열 형태)
+    prediction_map = {0: "1", 1: "2", 2: "3", 3: "4", 4: "5"}
+    predictions = [prediction_map[idx] for idx in predicted_indices]
+    return predictions
+
+
+def preprocess_labels(labels, tokenizer: PreTrainedTokenizerFast):
+    """
+    레이블 데이터를 전처리하는 함수.
+    """
+    labels = np.where(labels != -100, labels, tokenizer.pad_token_id)
+    labels = tokenizer.batch_decode(labels, skip_special_tokens=True)
+    return [x.strip() for x in labels]
