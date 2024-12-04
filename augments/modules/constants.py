4dd704e32cbf715461cc087585a3ecb5656df59e
Gamchan Kang <104420634+gsgh3016@users.noreply.github.com>
2024-12-04 15:59:01 +0900

    feat: 기존 데이터 칼럼 명 추가

--- augments/modules/constants.py
+++ augments/modules/constants.py
@@ -1,7 +1,14 @@
 # pd.DataFrame의 column 이름으로 사용되는 상수입니다.
+PARAGRAPH = "paragraph"
+QUESTION = "question"
+CHOICES = "choices"
+ANSWER = "answer"
+QUESTION_PLUS = "question_plus"
 REASONING = "reasoning"
 ANALYSIS = "analysis"
 CATEGORY = "category"
+
+# 조합하여 column 이름으로 사용되는 상수입니다. ex) keyword_1_exists
 KEYWORD_PREFIX = "keyword_"
 EXISTS_SUFFIX = "_exists"
 PAGE_SUFFIX = "_page"
@@ -10,3 +17,7 @@ SUMMARY_SUFFIX = "_summary"
 # 프롬프트에 사용되는 변수 명입니다.
 KEYWORDS = "keywords"
 DOCUMENT = "document"
+
+# Langchain RunnableMap에서 사용되는 key 값입니다.
+ORIGINAL_INPUTS = "original_inputs"
+REASONING_OUTPUT = "reasoning_output"
