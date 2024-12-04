ffc3c2a98c235bc1701fd58f77996a960052dfe4
Gamchan Kang <104420634+gsgh3016@users.noreply.github.com>
2024-12-04 15:59:00 +0900

    refactor: 이전 #17 실험 결과 파일 모듈 수정
    - `utils` 모듈에 실험 결과 파일명 생성 함수 추가
    - `prompts/templates` 디렉토리 내부에 `data_classification` 디렉토리 생성
    - 프롬프트 `solving_strategies.txt` 파일 생성
    - `eda/images` 디렉토리 생성
    - `category_distribution.png` 파일 생성
    - `problem_type_analysis.ipynb`에서 `problem_solving_type_analysis.ipynb`으로 파일 이름 변경
    - `problem_solving_type_analysis.ipynb` 코드에 변경 및 추가된 모듈 적용
    - `data/experiment`에서 `data/experiments`로 디렉토리 이름 변경

--- utils/util.py
+++ utils/util.py
@@ -24,3 +24,18 @@ def str_to_dtype(dtype_str: str):
         return torch.bfloat16
     else:
         raise ValueError(f"Unsupported dtype: {dtype_str}")
+
+
+def create_experiment_file_name(target_file: str, version: str, file_format="csv") -> str:
+    """실험 결과 파일 명을 생성하는 함수
+
+    Args:
+        target_file (str): 실험에 사용한 파일
+        version (str): _description_
+        file_format (str, optional): _description_. Defaults to "csv".
+
+    Returns:
+        str: _description_
+    """
+    prefix = target_file.split("_")[0]
+    return prefix + "_" + version + "." + file_format
