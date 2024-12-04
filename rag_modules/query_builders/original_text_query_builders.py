df10aff0c42f1cd9fabd15b5147488f73ebf08b0
canola <canolayoo78@gmail.com>
2024-12-04 16:21:33 +0900

    feat: 감찬 추출 keyword query builder 구현
    아래 두 가지 builder 구현
    위키피디아 페이지가 존재하는 단어만 query로 빌드하는 builder
    위키피디아 페이지 존재 여부 상관 없이 모두 query로 빌드하는 builder
    
    Fixes #54, #40

--- rag_modules/query_builders/original_text_query_builders.py
+++ rag_modules/query_builders/original_text_query_builders.py
@@ -21,3 +21,21 @@ class original_choices_query_builder(query_builder):
 class original_question_plus_query_builder(query_builder):
     def build(self, row: pd.Series) -> str:
         return row.get("question_plus", "")
+
+
+class original_keywords_query_builder(query_builder):
+    def build(self, row: pd.Series) -> str:
+        try:
+            # 'keywords'이 df.columns에 없으면 KeyError가 발생
+            return " ".join(keyword for keyword in row["keywords"])
+        except KeyError:
+            print("Error: keywords column이 DataFrame에 존재하지 않습니다.")
+
+
+class original_exist_keywords_query_builder(query_builder):
+    def build(self, row: pd.Series) -> str:
+        try:
+            # 'keywords'이 df.columns에 없으면 KeyError가 발생
+            return " ".join(keyword for keyword, exists in zip(row["keywords"], row["keywords_exists"]) if exists == 1)
+        except KeyError:
+            print("Error: keywords column이 DataFrame에 존재하지 않습니다.")
