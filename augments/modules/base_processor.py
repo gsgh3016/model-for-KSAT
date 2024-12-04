2250d7877ef82a3d380bad58f845b4698ba4a57e
Gamchan Kang <104420634+gsgh3016@users.noreply.github.com>
2024-12-04 15:59:01 +0900

    docs: `BaseProcessor` docstring 추가

--- augments/modules/base_processor.py
+++ augments/modules/base_processor.py
@@ -23,9 +23,25 @@ class BaseProcessor(ABC):
         pass
 
     def sample_data(self, n: int, seed: int = 1004):
+        """
+        랜덤하게 샘플링해서 데이터를 저장하도록 하는 함수
+
+        Args:
+            n (int): 샘플링한 데이터 개수
+            seed (int, optional): 설정할 시드 값. 기본은 1004.
+        """
         self.source_data = self.source_data.sample(n=n, random_state=seed)
 
     def inject_source_data(self, data: pd.DataFrame):
+        """
+        작업할 데이터를 입력하는 함수
+
+        Args:
+            data (pd.DataFrame): 입력할 데이터
+
+        Raises:
+            TypeError: pd.DataFrame 형식으로 데이터를 입력하지 않은 경우 발생
+        """
         if not isinstance(data, pd.DataFrame):
             raise TypeError("data must be pandas.DataFrame format")
         self.source_data = data
