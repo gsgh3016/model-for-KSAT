041b4e70987b9bd0cd7a54cdfaced2f3f249a3cf
canola <canolayoo78@gmail.com>
2024-12-04 16:21:33 +0900

    refactor: tf-idf query builder 주석 추가
    Fixes #54 #40

--- rag_preprocess/query_builders/advanced_query_builders.py
+++ rag_preprocess/query_builders/advanced_query_builders.py
@@ -8,18 +8,17 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 
 from .base_query_builder import query_builder
 
-kiwi = Kiwi()
-
 
 # Kiwi 기반 커스텀 토크나이저 정의 (명사, 미분류)
 def kiwi_tokenizer(text):
+    kiwi = Kiwi()
     cleaned_text = re.sub(r"[^\w\s]", "", text)
     tokens = kiwi.tokenize(cleaned_text)
 
     # tokenize 결과 중 일반명사, 고유명사만 추출
     nouns = [token.form for token in tokens if token.tag in ["NNG", "NNP"]]
 
-    # 형태소로 분류되지 않은 것 중 길이가 2 이상인 단어 포함
+    # 품사 태그로 분류되지 않은 분석 불능 단어 중 길이가 2 이상인 단어 포함
     additional_candidates = [token.form for token in tokens if len(token.form) >= 2 and token.tag in ["UN"]]
     return list(nouns + additional_candidates)
 
@@ -27,16 +26,27 @@ def kiwi_tokenizer(text):
 class tf_idf_query_builder(query_builder):
     def __init__(self, columns: Union[str, List[str]], top_k: int = 5):
         """
-        :param top_k: TF-IDF 상위 키워드 개수
+        TF-IDF query builder 초기화
+
+        Args:
+            columns (Union[str, List[str]]):
+                TF-IDF 계산에 사용할 열
+                단일 열 이름이나 열 이름 목록을 받을 수 있다.
+            top_k (int):
+                TF-IDF 점수를 기준으로 추출할 상위 키워드 개수. 기본값은 5
         """
         self.columns = columns
         self.top_k = top_k  # 키워드 상위 개수
 
     def _extract_keywords(self, text: str) -> list[str]:
         """
-        TF-IDF로 중요한 키워드 추출.
-        :param text: 분석할 텍스트
-        :return: 상위 키워드 리스트
+        TF-IDF를 사용하여 중요한 키워드 추출
+
+        Args:
+            text (str): 분석할 텍스트
+
+        Return:
+            list[str]: TF-IDF 점수가 높은 상위 키워드 리스트
         """
         # TF-IDF 모델 생성
         vectorizer = TfidfVectorizer(max_features=50, tokenizer=kiwi_tokenizer)
@@ -52,12 +62,12 @@ class tf_idf_query_builder(query_builder):
 
     def build(self, row: pd.Series) -> str:
         """
-        TF-IDF 키워드를 활용해 Query 생성.
+        TF-IDF를 통해 추출한 키워드를 연결한 query 생성
         """
         # 지문에서 키워드 추출
         text = "".join(map(str, row[self.columns]))
         keywords = self._extract_keywords(text)
-        print(text)
-        keywords_text = " ".join(keywords)
 
-        return keywords_text
+        query = " ".join(keywords)
+
+        return query
