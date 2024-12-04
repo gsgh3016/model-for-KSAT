61fe9efa94d94822483684795820d458e31cd241
canola <canolayoo78@gmail.com>
2024-12-04 16:20:45 +0900

    fix: choices list 처리
    Fixes #54, #40

--- rag_modules/query_builders/original_text_query_builders.py
+++ rag_modules/query_builders/original_text_query_builders.py
@@ -1,3 +1,5 @@
+from ast import literal_eval
+
 import pandas as pd
 
 from .base_query_builder import query_builder
@@ -15,7 +17,9 @@ class original_question_query_builder(query_builder):
 
 class original_choices_query_builder(query_builder):
     def build(self, data: pd.Series) -> str:
-        return " ".join(data.get("choices", []))
+        choices = literal_eval(data.get("choices", ""))
+        choices_string = "\n".join(choices)
+        return choices_string
 
 
 class original_question_plus_query_builder(query_builder):
