41656c85451b90d516efc25678407b4068a4e5b6
Chell <ch.chell39@gmail.com>
2024-12-04 16:20:45 +0900

    style: 함수 선언문 정리 및 `if __name__ ==` 추가

--- wiki_data.py
+++ wiki_data.py
@@ -3,18 +3,10 @@ import os
 import sys
 import uuid
 
-# import wikipediaapi
 from tqdm import tqdm
 
 from rag_preprocess import generate_embeddings, load_document, split_into_chunks
 from utils import get_pinecone_index
-
-csv.field_size_limit(sys.maxsize)
-# CSV 파일 경로
-file_path = "data/wiki_contents_categories_from_test_exp_v1.0.3.csv"
-
-# 제외할 소제목 키워드 정의
-excluded_subtitle = ["각주", "같이 보기", "참고 자료", "외부 링크"]
 
 
 # 제외할 소제목 이전만 남기는 함수
@@ -25,36 +17,6 @@ def clean_dic(data_dict):
             text = text.split(keyword)[0]  # 키워드를 기준으로 자르고 첫 번째 부분만 남김
     data_dict["text"] = text.strip()  # 공백 제거 후 업데이트
     return data_dict
-
-
-# CSV를 딕셔너리로 읽기
-data = []
-with open(file_path, mode="r", encoding="utf-8") as file:
-    reader = csv.DictReader(file)
-    for row in reader:
-        # 각 dict값의 본문에서 필요없는 소제목을 삭제한 뒤 리스트에 추가
-        cleaned_row = clean_dic(dict(row))
-        data.append(cleaned_row)
-
-# 소제목 커팅 결과 확인
-for row in data[:5]:  # 첫 5개 데이터 출력
-    print("===============")
-    print(row["text"])
-
-
-for i in range(len(data)):
-    data[i]["page_index"] = i
-
-# 총 읽은 문서 수 확인
-print(data[-1]["page_index"])
-
-sys.path.append("..")
-
-
-raw_data = data
-
-# Pinecone index에 연결
-pinecone_index, index_name = get_pinecone_index()
 
 
 def rag_preprocessing(page_title, page_index):
@@ -91,110 +53,145 @@ def rag_preprocessing(page_title, page_index):
     # print(f"Upserted {len(embeddings)} embeddings with metadata to Pinecone index '{index_name}'.")
 
 
-save_path = "data"
-continue_flag = 0
+if __name__ == "__main__":  # noqa: C901
+    csv.field_size_limit(sys.maxsize)
+    # CSV 파일 경로
+    file_path = "data/wiki_contents_categories_from_test_exp_v1.0.3.csv"
 
-for i in tqdm(raw_data[400:800], desc="400~800"):
-    for key in list(i.keys()):
-        if not i[key]:
-            continue_flag = 1
-            break
-    if continue_flag == 1:
-        continue_flag = 0
-        continue
+    # 제외할 소제목 키워드 정의
+    excluded_subtitle = ["각주", "같이 보기", "참고 자료", "외부 링크"]
 
-    text = i["text"]
-    page_index = i["page_index"]
-    page_title = i["title"]
-    file_name = "wiki_text_upsert.txt"
-    os.makedirs(save_path, exist_ok=True)  # 디렉터리 없으면 생성
-    file_path = os.path.join(save_path, file_name)
+    # CSV를 딕셔너리로 읽기
+    data = []
+    with open(file_path, mode="r", encoding="utf-8") as file:
+        reader = csv.DictReader(file)
+        for row in reader:
+            # 각 dict값의 본문에서 필요없는 소제목을 삭제한 뒤 리스트에 추가
+            cleaned_row = clean_dic(dict(row))
+            data.append(cleaned_row)
 
-    with open(file_path, "w", newline="", encoding="utf-8") as file:
-        file.write(text)
+    # 소제목 커팅 결과 확인
+    for row in data[:5]:  # 첫 5개 데이터 출력
+        print("===============")
+        print(row["text"])
 
-    rag_preprocessing(page_title, page_index)
+    for i in range(len(data)):
+        data[i]["page_index"] = i
 
-for i in tqdm(raw_data[:400], desc="~400"):
-    for key in list(i.keys()):
-        if not i[key]:
-            continue_flag = 1
-            break
-    if continue_flag == 1:
-        continue_flag = 0
-        continue
+    # 총 읽은 문서 수 확인
+    print(data[-1]["page_index"])
 
-    text = i["text"]
-    page_index = i["page_index"]
-    page_title = i["title"]
-    file_name = "wiki_text_upsert.txt"
-    os.makedirs(save_path, exist_ok=True)  # 디렉터리 없으면 생성
-    file_path = os.path.join(save_path, file_name)
+    sys.path.append("..")
 
-    with open(file_path, "w", newline="", encoding="utf-8") as file:
-        file.write(text)
+    raw_data = data
 
-    rag_preprocessing(page_title, page_index)
+    # Pinecone index에 연결
+    pinecone_index, index_name = get_pinecone_index()
 
-for i in tqdm(raw_data[800:1200], desc="800~1200"):
-    for key in list(i.keys()):
-        if not i[key]:
-            continue_flag = 1
-            break
-    if continue_flag == 1:
-        continue_flag = 0
-        continue
+    save_path = "data"
+    continue_flag = 0
 
-    text = i["text"]
-    page_index = i["page_index"]
-    page_title = i["title"]
-    file_name = "wiki_text_upsert.txt"
-    os.makedirs(save_path, exist_ok=True)  # 디렉터리 없으면 생성
-    file_path = os.path.join(save_path, file_name)
+    for i in tqdm(raw_data[400:800], desc="400~800"):
+        for key in list(i.keys()):
+            if not i[key]:
+                continue_flag = 1
+                break
+        if continue_flag == 1:
+            continue_flag = 0
+            continue
 
-    with open(file_path, "w", newline="", encoding="utf-8") as file:
-        file.write(text)
+        text = i["text"]
+        page_index = i["page_index"]
+        page_title = i["title"]
+        file_name = "wiki_text_upsert.txt"
+        os.makedirs(save_path, exist_ok=True)  # 디렉터리 없으면 생성
+        file_path = os.path.join(save_path, file_name)
 
-    rag_preprocessing(page_title, page_index)
+        with open(file_path, "w", newline="", encoding="utf-8") as file:
+            file.write(text)
 
-for i in tqdm(raw_data[1200:1600], desc="1200~1600"):
-    for key in list(i.keys()):
-        if not i[key]:
-            continue_flag = 1
-            break
-    if continue_flag == 1:
-        continue_flag = 0
-        continue
+        rag_preprocessing(page_title, page_index)
 
-    text = i["text"]
-    page_index = i["page_index"]
-    page_title = i["title"]
-    file_name = "wiki_text_upsert.txt"
-    os.makedirs(save_path, exist_ok=True)  # 디렉터리 없으면 생성
-    file_path = os.path.join(save_path, file_name)
+    for i in tqdm(raw_data[:400], desc="~400"):
+        for key in list(i.keys()):
+            if not i[key]:
+                continue_flag = 1
+                break
+        if continue_flag == 1:
+            continue_flag = 0
+            continue
 
-    with open(file_path, "w", newline="", encoding="utf-8") as file:
-        file.write(text)
+        text = i["text"]
+        page_index = i["page_index"]
+        page_title = i["title"]
+        file_name = "wiki_text_upsert.txt"
+        os.makedirs(save_path, exist_ok=True)  # 디렉터리 없으면 생성
+        file_path = os.path.join(save_path, file_name)
 
-    rag_preprocessing(page_title, page_index)
+        with open(file_path, "w", newline="", encoding="utf-8") as file:
+            file.write(text)
 
-for i in tqdm(raw_data[1600:], desc="1600~"):
-    for key in list(i.keys()):
-        if not i[key]:
-            continue_flag = 1
-            break
-    if continue_flag == 1:
-        continue_flag = 0
-        continue
+        rag_preprocessing(page_title, page_index)
 
-    text = i["text"]
-    page_index = i["page_index"]
-    page_title = i["title"]
-    file_name = "wiki_text_upsert.txt"
-    os.makedirs(save_path, exist_ok=True)  # 디렉터리 없으면 생성
-    file_path = os.path.join(save_path, file_name)
+    for i in tqdm(raw_data[800:1200], desc="800~1200"):
+        for key in list(i.keys()):
+            if not i[key]:
+                continue_flag = 1
+                break
+        if continue_flag == 1:
+            continue_flag = 0
+            continue
 
-    with open(file_path, "w", newline="", encoding="utf-8") as file:
-        file.write(text)
+        text = i["text"]
+        page_index = i["page_index"]
+        page_title = i["title"]
+        file_name = "wiki_text_upsert.txt"
+        os.makedirs(save_path, exist_ok=True)  # 디렉터리 없으면 생성
+        file_path = os.path.join(save_path, file_name)
 
-    rag_preprocessing(page_title, page_index)
+        with open(file_path, "w", newline="", encoding="utf-8") as file:
+            file.write(text)
+
+        rag_preprocessing(page_title, page_index)
+
+    for i in tqdm(raw_data[1200:1600], desc="1200~1600"):
+        for key in list(i.keys()):
+            if not i[key]:
+                continue_flag = 1
+                break
+        if continue_flag == 1:
+            continue_flag = 0
+            continue
+
+        text = i["text"]
+        page_index = i["page_index"]
+        page_title = i["title"]
+        file_name = "wiki_text_upsert.txt"
+        os.makedirs(save_path, exist_ok=True)  # 디렉터리 없으면 생성
+        file_path = os.path.join(save_path, file_name)
+
+        with open(file_path, "w", newline="", encoding="utf-8") as file:
+            file.write(text)
+
+        rag_preprocessing(page_title, page_index)
+
+    for i in tqdm(raw_data[1600:], desc="1600~"):
+        for key in list(i.keys()):
+            if not i[key]:
+                continue_flag = 1
+                break
+        if continue_flag == 1:
+            continue_flag = 0
+            continue
+
+        text = i["text"]
+        page_index = i["page_index"]
+        page_title = i["title"]
+        file_name = "wiki_text_upsert.txt"
+        os.makedirs(save_path, exist_ok=True)  # 디렉터리 없으면 생성
+        file_path = os.path.join(save_path, file_name)
+
+        with open(file_path, "w", newline="", encoding="utf-8") as file:
+            file.write(text)
+
+        rag_preprocessing(page_title, page_index)
