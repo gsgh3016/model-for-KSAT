42630c388447a74150d9e3c731fefd368b99a79e
canola <canolayoo78@gmail.com>
2024-12-04 16:21:33 +0900

    refactor: original_text_query_builders build 함수 수정
    자식 class들 간 code 중복성을 줄이도록 수정
    Fixes #54, #40

--- rag_modules/query_builders/original_text_query_builders.py
+++ rag_modules/query_builders/original_text_query_builders.py
@@ -3,39 +3,57 @@ import pandas as pd
 from .base_query_builder import query_builder
 
 
-class original_paragraph_query_builder(query_builder):
+class original_query_key_builder(query_builder):
+    def __init__(self, key: str = ""):
+        self.key = key
+
     def build(self, row: pd.Series) -> str:
-        return row.get("paragraph", "")
+        if self.key not in row.index:
+            raise KeyError(f"The key '{self.key}' does not exist in the provided row.")
+        return row.get(self.key)
 
 
-class original_question_query_builder(query_builder):
+class original_paragraph_query_builder(original_query_key_builder):
+    def __init__(self):
+        super().__init__(key="paragraph")
+
+
+class original_question_query_builder(original_query_key_builder):
+    def __init__(self):
+        super().__init__(key="question")
+
+
+class original_choices_query_builder(original_query_key_builder):
+    def __init__(self):
+        super().__init__(key="choices_text")
+
+
+class original_question_plus_query_builder(original_query_key_builder):
+    def __init__(self):
+        super().__init__(key="question_plus")
+
+
+class original_summarization_query_builder(original_query_key_builder):
+    def __init__(self):
+        super().__init__(key="summarization")
+
+
+class original_keywords_query_builder(original_query_key_builder):
+    def __init__(self):
+        super().__init__(key="keywords")
+
     def build(self, row: pd.Series) -> str:
-        return row.get("question", "")
+        keywords = super().build(row)
+        return " ".join(keyword for keyword in keywords)
 
 
-class original_choices_query_builder(query_builder):
+class original_exist_keywords_query_builder(original_query_key_builder):
+    def __init__(self):
+        super().__init__(key="keywords")
+
     def build(self, row: pd.Series) -> str:
-        return row.get("choices_text", "")
+        keywords = super().build(row)
 
-
-class original_question_plus_query_builder(query_builder):
-    def build(self, row: pd.Series) -> str:
-        return row.get("question_plus", "")
-
-
-class original_keywords_query_builder(query_builder):
-    def build(self, row: pd.Series) -> str:
-        try:
-            # 'keywords'이 df.columns에 없으면 KeyError가 발생
-            return " ".join(keyword for keyword in row["keywords"])
-        except KeyError:
-            print("Error: keywords column이 DataFrame에 존재하지 않습니다.")
-
-
-class original_exist_keywords_query_builder(query_builder):
-    def build(self, row: pd.Series) -> str:
-        try:
-            # 'keywords'이 df.columns에 없으면 KeyError가 발생
-            return " ".join(keyword for keyword, exists in zip(row["keywords"], row["keywords_exists"]) if exists == 1)
-        except KeyError:
-            print("Error: keywords column이 DataFrame에 존재하지 않습니다.")
+        self.key = "keywords_exists"
+        keywords_exists = super().build(row)
+        return " ".join(keyword for keyword, exists in zip(keywords, keywords_exists) if exists == 1)
